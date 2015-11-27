9ee97c2dd5fe8cfcdc4cd51dba23b69079445b14
Thyago Stall de Araujo <thyago@fouritil.com.br>
2015-11-27 11:36:05 -0200

    RecyclerView, Cards and StaggeredLayout

---
+++ app/src/main/java/com/thyago/androidplayground/RecyclerViewActivity.java
@@ -0,0 +1,91 @@
+package com.thyago.androidplayground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public RelativeLayout mRelativeLayout;
+            public ViewHolder(RelativeLayout v) {
+                super(v);
+                mRelativeLayout = v;
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recycler_view_item, parent, false);
+            // set the view's size, margins, paddings and layout parameters
+
+            ViewHolder vh = new ViewHolder((RelativeLayout) v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            TextView v = (TextView) holder.mRelativeLayout.findViewById(R.id.text_item_recycler);
+            v.setText(mDataset[position]);
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+    }
+
+    protected String[] getDataSet(int number) {
+        String[] result = new String[number];
+        for (int i = 0; i < number; i++) {
+            result[i] = "Item: " + (i + 1);
+        }
+        return result;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(5, StaggeredGridLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String[] dataSet = getDataSet(50);
+
+        MyAdapter adapter = new MyAdapter(dataSet);
+        recyclerView.setAdapter(adapter);
+    }
+}
