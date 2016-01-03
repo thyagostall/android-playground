864dadddb22f05b1e47cdee7f6fa5b0510f1cfd7
thyagostall <thstall@gmail.com>
2016-01-03 00:24:27 -0200

    Added project MovieBoss: Final Project of Developing Android Apps

---
+++ MovieBoss/app/src/main/java/com/thyago/movieboss/API.java
@@ -0,0 +1,7 @@
+package com.thyago.movieboss;
+
+/**
+ * Created by thyago on 1/3/16.
+ */
+public class API {
+}
---
+++ MovieBoss/app/src/main/java/com/thyago/movieboss/Movie.java
@@ -0,0 +1,14 @@
+package com.thyago.movieboss;
+
+import java.util.Date;
+
+/**
+ * Created by thyago on 1/3/16.
+ */
+public class Movie {
+    private String title;
+    private String originalTitle;
+    private int rating;
+    private Date releaseDate;
+    private String plot;
+}
