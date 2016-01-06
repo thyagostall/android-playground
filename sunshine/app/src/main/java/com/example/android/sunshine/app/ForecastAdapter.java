62eaf9f72b1bc2055995b5586ffb4b5c2a562e47
thyagostall <thstall@gmail.com>
2016-01-06 14:20:08 -0200

    Changes in layout

--- sunshine/app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ sunshine/app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -57,7 +57,7 @@ public class ForecastAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
-        TextView tv = (TextView) view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+//        TextView tv = (TextView) view;
+//        tv.setText(convertCursorRowToUXFormat(cursor));
     }
 }
